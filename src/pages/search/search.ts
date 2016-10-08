61bc56e04d68ceaa4f78ffa207288b18085d0a4d
Daniel Graaf <dg1613@ic.ac.uk>
2016-10-08 15:59:06 +0100

    No idea anymore

---
+++ src/pages/search/search.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'search.html'
+})
+
+export class SearchPage {
+
+    constructor() {
+        console.log("Hello world!");
+    }
+
+}
